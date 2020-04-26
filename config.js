a1c93db3769c3e4616c82cfe8e8a4de691f93de6
Mat Pustizzi <mathew.pustizzi@gmail.com>
2020-04-26 16:34:16 -0400

    revert timeout changes to app

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
 const errorHandler = require("errorhandler");
 const path = require("path");
-const timeout = require("connect-timeout");
 
 module.exports = (() => {
   const app = express();
@@ -17,20 +16,12 @@ module.exports = (() => {
   app.set("views", path.join(__dirname, "views"));
   app.set("view engine", "pug");
   app.use(logger("dev"));
-  app.use(timeout("5s"));
   app.use(bodyParser.json());
-  app.use(haltOnTimedout);
   app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(haltOnTimedout);
   app.use(methodOverride());
-  app.use(haltOnTimedout);
   app.use(express.static(path.join(__dirname, "public")));
-  app.use(haltOnTimedout);
-  app.use(errorHandler());
 
-  function haltOnTimedout(req, res, next) {
-    if (!req.timedout) next();
-  }
+  app.use(errorHandler());
 
   return app;
 })();
