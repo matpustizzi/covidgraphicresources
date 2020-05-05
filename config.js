80049588afe0cc7377574bc782e585de1f0aa6ac
Mat Pustizzi <mathew.pustizzi@gmail.com>
2020-05-05 18:47:47 -0400

    move timeout to end

--- config.js
+++ config.js
@@ -16,14 +16,12 @@ module.exports = (() => {
   app.set("port", process.env.PORT || 3000);
   app.set("views", path.join(__dirname, "views"));
   app.set("view engine", "pug");
-  app.use(timeout.handler);
   app.use(logger("dev"));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(methodOverride());
   app.use(express.static(path.join(__dirname, "public")));
-
   app.use(errorHandler());
-
+  app.use(timeout.handler);
   return app;
 })();
